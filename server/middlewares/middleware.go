9950e98fba3e03f21214b755c0c4a0ff71c1838f
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-24 23:18:18 +0530

    - API for GET, POST, PUT requests.

--- server/middlewares/middleware.go
+++ server/middlewares/middleware.go
@@ -1,1 +1,96 @@
 package middlewares
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/krishnalagad/golang-react-todo/models"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var collection *mongo.Collection
+
+func init() {
+	loadTheEnv()
+	createDBInstance()
+}
+
+func loadTheEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createDBInstance() {
+	connectionString := os.Getenv("DB_URI")
+	dbName := os.Getenv("DB_NAME")
+	collName := os.Getenv("DB_COLLECTION_NAME")
+
+	clientOptions := options.Client().ApplyURI(connectionString)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB.")
+
+	collection = client.Database(dbName).Collection(collName)
+	fmt.Println("Collection instance created.")
+}
+
+func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	payload := getAllTasks()
+	json.NewEncoder(w).Encode(payload)
+}
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	var task models.ToDoList
+	json.NewDecoder(r.Body).Decode(&task)
+	insertOneTask(task)
+	json.NewEncoder(w).Encode(task)
+}
+
+func TaskComplete(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "PUT")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	params := mux.Vars(r)
+	taskComplete(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+}
+
+func UndoTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteAllTasks(w http.ResponseWriter, r *http.Request) {
+
+}
