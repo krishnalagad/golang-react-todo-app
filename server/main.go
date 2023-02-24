1e54b001c25fb063b5c3b113a9d0716015b5252a
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-24 21:48:39 +0530

    - created model, router

--- server/main.go
+++ server/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/krishnalagad/golang-react-todo/router"
+)
+
+func main() {
+	fmt.Println("Starting project on port 9000")
+
+	r := router.Router()
+
+	log.Fatal(http.ListenAndServe(":9000", r))
+}
--- server/router/router.go
+++ server/router/router.go
@@ -1,1 +1,19 @@
 package router
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/krishnalagad/golang-react-todo/middlewares"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	
+	router.HandleFunc("/api/task", middlewares.GetAllTasks).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/tasks", middlewares.CreateTask).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/tasks/{id}", middlewares.TaskComplete).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/undotask/{id}", middlewares.UndoTask).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/deletetask/{id}", middlewares.DeleteTask).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/deleteAllTasks", middlewares.DeleteAllTasks).Methods("DELETE", "OPTIONS")
+
+	return router
+}
